8de82e45ddbda75761d8a425c3a8f94a67b46ea9
Kevin <kevinscottsciliento@gmail.com>
2019-03-20 16:35:10 -0400

    Ajout du travail sur door

--- data/door.cpp
+++ data/door.cpp
@@ -1,9 +1,12 @@
+/****************************************************************************
+ * Fichier:			door.cpp
+ * Auteurs: 		Kevin Ciliento, Gabriel Tagliabracci et Roman Zhornytskiy
+ * Date:			20 mars 2019
+ * Description:		Implementation de la classe Door
+ ****************************************************************************/
+
 #include "door.h"
 
-class Door {
-
-
-
-
-
-};
+Door::Door(unsigned doorNumber) {
+	doorName_ = "\\text_files\\" + std::to_string(doorNumber) + ".txt";
+}
--- data/door.h
+++ data/door.h
@@ -1,4 +1,13 @@
-#pragma once
+/****************************************************************************
+ * Fichier:			door.h
+ * Auteurs: 		Kevin Ciliento, Gabriel Tagliabracci et Roman Zhornytskiy
+ * Date:			20 mars 2019
+ * Description:		Definition de la classe Door
+ ****************************************************************************/
+
+#ifndef DOOR_H
+#define DOOR_H
+#include <fstream>
 
 #include <vector>
 #include <string>
@@ -8,11 +17,18 @@ using namespace std;
 class Door {
 
 public:
-	Door();
+	Door(void) = default;
+	Door(unsigned doorNumber);
+	~Door() { file.close(); };
+
+	bool readFile();
 
 private:
-
-	vector<string> rules;
-	vector<string> passwords;
+	string doorName_;
+	vector<string> rules_;
+	vector<string> passwords_;
+	fstream file;
 
 };
+
+#endif // !DOOR_H
